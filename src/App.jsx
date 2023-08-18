6ae1b7fc6540e0943c2064a965597731309f28ed
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-18 14:33:24 -0500

    Fix
    Fix linters

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Calculatorb from './components/Calculator'
 
 function App() {
   return (
-    <div className = 'App'>
-      <div className = 'Tittle-container'>
-        <h1 className = 'tittle'>Math Magicians</h1>
+    <div className ="App">
+      <div className="Tittle-container">
+        <h1 className="tittle">Math Magicians</h1>
       </div>
       <Calculatorb />
     </div>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import '../StyleSheets/calculator.css';
+import React from "react";
+import "../StyleSheets/calculator.css";
 
 function Calculatorb() {
   return (
-    <div className= 'calculator-container'>
-      <input type= 'number' className= 'number-input' placeholder='0' />
-      <div className='buttons-container'>
-        <div className='numbers-container'>
-          <button className='calculator-button'>AC</button>
-          <button className='calculator-button'>+/-</button>
-          <button className='calculator-button'>%</button>
-          <button className='calculator-button'>7</button>
-          <button className='calculator-button'>8</button>
-          <button className='calculator-button'>9</button>
-          <button className='calculator-button'>4</button>
-          <button className='calculator-button'>5</button>
-          <button className='calculator-button'>6</button>
-          <button className='calculator-button'>1</button>
-          <button className='calculator-button'>2</button>
-          <button className='calculator-button'>3</button>
-          <button className='calculator-button zero'>0</button>
-          <button className='calculator-button'>.</button>
+    <div className="calculator-container">
+      <input type="number" className="number-input" placeholder="0" />
+      <div className="buttons-container">
+        <div className="numbers-container">
+          <button className="calculator-button" type="button">AC</button>
+          <button className="calculator-button" type="button">+/-</button>
+          <button className="calculator-button" type="button">%</button>
+          <button className="calculator-button" type="button">7</button>
+          <button className="calculator-button" type="button">8</button>
+          <button className="calculator-button" type="button">9</button>
+          <button className="calculator-button" type="button">4</button>
+          <button className="calculator-button" type="button">5</button>
+          <button className="calculator-button" type="button">6</button>
+          <button className="calculator-button" type="button">1</button>
+          <button className="calculator-button" type="button">2</button>
+          <button className="calculator-button" type="button">3</button>
+          <button className="calculator-button zero" type="button">0</button>
+          <button className="calculator-button" type="button">.</button>
         </div>
-        <div className='basic-operations-container'>
-          <button className='calculator-button operator'>/</button>
-          <button className='calculator-button operator'>x</button>
-          <button className='calculator-button operator'>-</button>
-          <button className='calculator-button operator'>+</button>
-          <button className='calculator-button operator'>=</button>
+        <div className="basic-operations-container">
+          <button className="calculator-button operator" type="button">/</button>
+          <button className="calculator-button operator" type="button">x</button>
+          <button className="calculator-button operator" type="button">-</button>
+          <button className="calculator-button operator" type="button">+</button>
+          <button className="calculator-button operator" type="button">=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Calculatorb;
