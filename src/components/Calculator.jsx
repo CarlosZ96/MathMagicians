fda89f48d5ec875c53daef98527dc1a8e8fff492
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-18 22:45:25 -0500

    Feacture:
    The necessary imports are added, the onClick prop and the event listener are added to update the input according to the results shown by the added calculate and operate files.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../StyleSheets/calculator.css';
 import Button from './boton';
+import calculate from '../logic/calculate';
 
 function Calculatorb() {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newCalculatorData = calculate(calculatorData, buttonName);
+    setCalculatorData(newCalculatorData);
+  };
+
   return (
     <div className="calculator-container">
-      <input type="number" className="number-input" placeholder="0" />
+      <input
+        type="text"
+        className="number-input"
+        value={calculatorData.next || calculatorData.total || '0'}
+        readOnly
+      />
       <div className="buttons-container">
         <div className="numbers-container">
-          <Button className="calculator-button">AC</Button>
-          <Button className="calculator-button">+/-</Button>
-          <Button className="calculator-button">%</Button>
-          <Button className="calculator-button">7</Button>
-          <Button className="calculator-button">8</Button>
-          <Button className="calculator-button">9</Button>
-          <Button className="calculator-button">4</Button>
-          <Button className="calculator-button">5</Button>
-          <Button className="calculator-button">6</Button>
-          <Button className="calculator-button">1</Button>
-          <Button className="calculator-button">2</Button>
-          <Button className="calculator-button">3</Button>
-          <Button className="calculator-button zero">0</Button>
-          <Button className="calculator-button">.</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('AC')}>AC</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('+/-')}>+/-</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('%')}>%</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('7')}>7</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('8')}>8</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('9')}>9</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('4')}>4</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('5')}>5</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('6')}>6</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('1')}>1</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('2')}>2</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('3')}>3</Button>
+          <Button className="calculator-button zero" onClick={() => handleButtonClick('0')}>0</Button>
+          <Button className="calculator-button" onClick={() => handleButtonClick('.')}>.</Button>
         </div>
         <div className="basic-operations-container">
-          <Button className="calculator-button operator">÷</Button>
-          <Button className="calculator-button operator">x</Button>
-          <Button className="calculator-button operator">-</Button>
-          <Button className="calculator-button operator">+</Button>
-          <Button className="calculator-button operator">=</Button>
+          <Button className="calculator-button operator" onClick={() => handleButtonClick('÷')}>÷</Button>
+          <Button className="calculator-button operator" onClick={() => handleButtonClick('x')}>x</Button>
+          <Button className="calculator-button operator" onClick={() => handleButtonClick('-')}>-</Button>
+          <Button className="calculator-button operator" onClick={() => handleButtonClick('+')}>+</Button>
+          <Button className="calculator-button operator" onClick={() => handleButtonClick('=')}>=</Button>
         </div>
       </div>
     </div>
