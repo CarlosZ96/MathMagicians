2281c2432e37b119c265ae6236e93c9f1d055ebd
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-19 17:42:36 -0500

    Feature RandomQuote
    the functionality of the api is changed to
    randomquote and i adden in the div for the calculator and de quote.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
 import Calculatorb from './components/Calculator';
-import emoji from './components/quote';
+import RandomQuote from './components/quote';
 
 function App() {
-  const [quoteData, setQuoteData] = useState(null);
 
-  useEffect(() => {
-    emoji(setQuoteData);
-  }, []);
+
 
   return (
     <div className="App">
       <div className="Title-container">
-        <h1 className="title">Math Magicians</h1>
-        {quoteData && (
-          <div className="quote">
-            <span className="emoji">{quoteData.character}</span>
-          </div>
-        )}
+        <h1>Math-Magicians</h1>
       </div>
+      <div className='appContainer'>
       <Calculatorb />
+      <RandomQuote />
+      </div>
     </div>
   );
 }
--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -1,24 +1,55 @@
-async function emoji(callback) {
-  const name = 'slightly%20smiling%20face';
-  const apiKey = '2VyJFEAwAfCwJeQRTJpUKw==qoL1KftFFfAoQ4th';
+import React from 'react';
 
-  try {
-    const response = await fetch(`https://api.api-ninjas.com/v1/emoji?name=${name}`, {
+const RandomQuote = () => {
+  const [quote, setQuote] = React.useState([]);
+  const [isLoading, setIsLoaded] = React.useState(false);
+  const [hasError, setHasError] = React.useState(null);
+  const getQuote = async () => {
+    setIsLoaded(true);
+    const category = 'knowledge';
+    await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      method: 'GET',
       headers: {
-        'X-Api-Key': apiKey,
+        'X-Api-Key': '2VyJFEAwAfCwJeQRTJpUKw==qoL1KftFFfAoQ4th',
+        'Content-Type': 'application/json',
       },
+    }).then((response) => response.json()).then((data) => {
+      setQuote(data);
+    }).catch((error) => {
+      setHasError(error);
+      setIsLoaded(false);
     });
+    setIsLoaded(false);
+  };
 
-    if (!response.ok) {
-      console.error('Error:', response.status, response.statusText);
-      return;
-    }
+  React.useEffect(() => {
+    getQuote();
+  }, []);
 
-    const data = await response.json();
-    callback(data[0]);
-  } catch (error) {
-    console.error('Request failed:', error);
+  if (isLoading) {
+    return (
+      <div className="quote-loading">
+        <p>  Loading...</p>
+      </div>
+    );
+  } if (hasError) {
+    return (
+      <div className="quote-error">
+        <p>Something went wrong...</p>
+      </div>
+    );
   }
-}
 
-export default emoji;
+  return (
+    <div className="quote">
+      {quote?.map((item) => (
+        <div key={item.id + 1}>
+          <p className="quote-text">{item.quote}</p>
+          <p className="quote-author">{item.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RandomQuote;
